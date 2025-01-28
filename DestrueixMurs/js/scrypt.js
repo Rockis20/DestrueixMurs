30eb2e5a6dffdde2771788b763ea084e33c50fee
Rockis20 <rherrero20056@iesjoanramis.org>
2025-01-28 12:52:12 +0100

    Add files via upload

--- DestrueixMurs/js/scrypt.js
+++ DestrueixMurs/js/scrypt.js
@@ -1,2 +1,67 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
+
+canvas.height = 512;
+canvas.width = 448;
+
+// Variables Pilotes
+const radiPilota = 4;
+
+let x = canvas.width / 2
+let y = canvas.height - 30
+
+//Velocitat PILOTA
+let dx = 2
+let dy = -2
+
+function pintarPilota(){
+    ctx.beginPath();
+    ctx.arc(x, y, radiPilota, 0, Math.PI*2);
+    ctx.fillStyle = "#FFF";
+    ctx.fill();
+    ctx.closePath();
+}
+function pintarPala(){
+
+
+}
+function pintarMurs(){
+
+
+}
+function detecciColisio(){
+
+
+}
+function movimentPilota(){
+    if(x >= canvas.width|| x<=0){
+        dx=-dx
+    }
+    if(y >= canvas.height || y<=0){
+        dy=-dy
+    }
+    x += dx
+    y += dy
+}
+function movimentPala(){
+
+
+}
+function borrarPantalla(){
+    canvas.height = 512;
+    canvas.width = 448;
+}
+
+function pintarCanvas(){
+    console.log("Hola");
+    borrarPantalla();
+    pintarPilota();
+    pintarPala();
+    pintarMurs();
+    detecciColisio();
+    movimentPilota();
+    movimentPala();
+    window.requestAnimationFrame(pintarCanvas);
+}
+
+pintarCanvas();
